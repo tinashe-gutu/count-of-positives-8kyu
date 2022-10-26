c5f9e299f02961dcfa7831cf163beb785c87d8af
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-26 11:50:14 +0100

    add JS file with JSdoc for countSum function declaration

---
+++ src/count-of-positives.js
@@ -0,0 +1,8 @@
+/**
+ * returns count of positives and sum of negatives
+ * @param {number[]} numArr array of postive and negative nums
+ * @returns {number[]} tupple of count and sum
+ */
+function countSum(numArr){
+   
+}
